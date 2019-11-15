7f6332c0ec9d4ef421c83ccd94065676431c037e
Justin Yun <justinyun@google.com>
2019-11-15 16:12:04 +0900

    Inherit base system_ext.mk files for aosp targets
    Make aosp targets inherit handheld_system_ext.mk and
    telephony_system_ext.mk files.
    Devices that have /system_ext or /system/system_ext must inherit any
    of *_system_ext.mk files to install mandatory packages for system_ext
    partition.
    
    Bug: 141443017
    Bug: 157548597
    Bug: 144542478
    Test: Build aosp targets
    Change-Id: I38bc273be0247e9613e196436d6efab79d2e6d23
    Merged-In: I38bc273be0247e9613e196436d6efab79d2e6d23
    (cherry picked from commit ff91e26b78b18d2ddffe29b6e4f1c837462d15f5)

--- aosp_RMX206X.mk
+++ aosp_RMX206X.mk
@@ -6,6 +6,10 @@
 
 $(call inherit-product, device/realme/RMX206X/device.mk)
 
+# All components inherited here go to system_ext image
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
+
 # AOSP Gapps
 $(call inherit-product, vendor/aosp/config/common.mk)
 
